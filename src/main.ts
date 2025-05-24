12584bac5a98e9d99dc7ba272990cac03e5085ff
he23inw3 <golden2finger@gmail.com>
2025-05-24 17:52:45 +0900

    GTagを検知しないので、確認用に追加

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,18 @@ import router from './router'
 import { vuetify } from './plugins/vuetify'
 import './style.css'
 
+const script = document.createElement('script')
+script.async = true
+script.src = `https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GTAG_ID}`
+document.head.appendChild(script)
+
+window.dataLayer = window.dataLayer || []
+function gtag(...args: any[]) {
+  window.dataLayer.push(args)
+}
+gtag('js', new Date())
+gtag('config', import.meta.env.VITE_GTAG_ID)
+
 // Initialize the app
 const app = createApp(App)
 
@@ -14,14 +26,14 @@ const pinia = createPinia()
 app.use(pinia)
 
 // Initialize and use Gtag
-const gtag = createGtag({
+const vueGtag = createGtag({
   tagId: import.meta.env.VITE_GTAG_ID,
   pageTracker: {
     router,
     useRouteFullPath: true
   }
 })
-app.use(gtag)
+app.use(vueGtag)
 
 // Initialize and use Vue Router
 app.use(router)
