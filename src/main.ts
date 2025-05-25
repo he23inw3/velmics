a7b8c39e150f18493376f70bda2c0cf9d7998a02
he23inw3 <golden2finger@gmail.com>
2025-05-25 22:24:49 +0900

    セミコロン付け忘れ分を追加

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import { vuetify } from './plugins/vuetify'
-import './style.css'
-import { initGtag, sendPageView } from './utils/gtag'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import { vuetify } from './plugins/vuetify';
+import './style.css';
+import { initGtag, sendPageView } from './utils/gtag';
 
 // Initialize the app
-const app = createApp(App)
+const app = createApp(App);
 
 // Set up Pinia for state management
-const pinia = createPinia()
-app.use(pinia)
+const pinia = createPinia();
+app.use(pinia);
 
 // Initialize and use Gtag
-initGtag()
+initGtag();
 
 router.afterEach((to) => {
-  sendPageView(to.fullPath)
-})
+  sendPageView(to.fullPath);
+});
 
 // Initialize and use Vue Router
-app.use(router)
+app.use(router);
 
 // Initialize and use Vuetify
 app.use(vuetify)
